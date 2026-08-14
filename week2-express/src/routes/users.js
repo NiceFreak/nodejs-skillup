b4a0dea6514cc2f68db03fa455e276693fc2b50c
nezha <writtenbylx@hotmail.com>
2026-08-14 14:45:16 +0800

    update: 升级 users 鉴权

--- week2-express/src/routes/users.js
+++ week2-express/src/routes/users.js
@@ -9,8 +9,13 @@ import {
 import { validateIdParam } from '../middlewares/validateIdParamMiddleware.js';
 import { validateHasRequestBody } from '../middlewares/validateHasRequestBodyMiddleware.js';
 import { validateStatusParam } from '../middlewares/validateStatusParamsMiddleware.js';
+import { validateToken } from '../middlewares/validateTokenMiddleware.js';
+import { requireRole } from '../middlewares/validateRoleMiddleware.js';
 
 const usersRouter = express.Router();
+
+// 统一守卫: 所有用户管理端点必须认证且具备 admin 角色
+usersRouter.use(validateToken, requireRole('admin'));
 
 // GET /users
 usersRouter.get('/', listUsersController);
